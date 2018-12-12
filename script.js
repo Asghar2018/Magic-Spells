b4a521eea075fb41001f54434ecd1cf54be378d2
Ash <44040571+Asghar2018@users.noreply.github.com>
2018-12-12 17:43:15 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,12 @@
+$(".Deletus").click(function(){
+    $("#ExplorerChan").fadeToggle();
+});
+
+$(".ChangeForm").click(function(){
+    $("#OPM").slideToggle();
+});
+$(".UltimateMove").click(function(){
+    $("#Explosion").toggle();
+});
+
+    
